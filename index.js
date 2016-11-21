343dc498df308c9f109f47d524ad958880728e0b
Mattheus Jucá <Mattheus Juc>
2016-11-21 12:01:37 -0200

    Comment code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+
+//Initialize a new instance of socket.io by passing the the HTTP server object.
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
@@ -9,13 +11,19 @@ app.get('/', function (req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// Listen on the connection event for incoming sockets.
 io.on('connection', function (socket){
+	// Log connected sockets to the console
   console.log('a user connected');
   socket.on('chat message', function (msg){
+  	// Log chat message
   	console.log('message: ' + msg);
+  	// Brodcasting message, send to everyone clients.
   	io.emit('chat message', msg);
  	});
+ 	// Listen on the disconnect event.
   socket.on('disconnect', function(){
+  	// Log that socket was closed.
  	  console.log('a user disconnected');
   });
 });
