9ff83421401176fbdc7f3c321c27cd11e9944c8e
Mattheus Jucá <Mattheus Juc>
2016-11-21 17:31:13 -0200

    User counter on Client

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,7 +2,7 @@
 // It defaults to trying to connect to the host that serves the page.
 var socket = io();
 
-// Get a typed message and send to server.
+// Get a typed message and send data message to server.
 $('form').submit(function(){
   socket.emit('chat message', $('#m').val());
   $('#m').val('');
@@ -14,7 +14,8 @@ socket.on('chat message', function (msg){
   $('#messages').append($('<li>').text(msg));
 });
 
-// When server call 'stats'.
+// When server call 'stats', client receive data with numUsers and print in page.
 socket.on('stats', function (data) {
-  console.log('Conected users:', data.numUsers)
+  console.log('Connected users:', data.numUsers)
+  $('#counter').html("Connected users: " + data.numUsers);
 });
